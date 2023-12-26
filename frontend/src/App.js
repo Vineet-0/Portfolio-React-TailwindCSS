31ffb8001668a3d00b2cd713f6f4d386bdcd5296
Vineet-0 <vineet351016@gmail.com>
2023-12-26 15:42:18 +0530

    first commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { AnimatePresence } from 'framer-motion';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import './index.css';
+
+import ScrollToTop from './components/ScrollToTop.jsx';
+import Header from './components/shared/Header.jsx'
+import Footer from './components/shared/Footer.jsx'
+import useScrollToTop from './hooks/useScrollToTop.jsx';
+
+const AboutMe = lazy(() => import('./pages/AboutMe.jsx'));
+const Contact = lazy(() => import('./pages/Contact.jsx'));
+const Home = lazy(() => import('./pages/Home.jsx'));
+const Projects = lazy(() => import('./pages/Projects.jsx'));
+const ProjectSingle = lazy(() => import('./pages/ProjectSingle.jsx'));
 
 function App() {
-  return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    </div>
-  );
+    return (
+        
+            <AnimatePresence>
+                <div className=" bg-secondary-light dark:bg-primary-dark transition duration-300">
+                    <div className="dot-grid-container w-full min-h-screen">
+                        <Router>
+                            <ScrollToTop />
+                            <Header />
+                            <Suspense fallback={""}>
+                                <Routes>
+                                    <Route path="/" element={<Home />} />
+                                    <Route path="projects" element={<Projects />} />
+                                    <Route
+                                        path="projects/single-project"
+                                        element={<ProjectSingle />}
+                                    />
+
+                                    <Route path="about" element={<AboutMe />} />
+                                    <Route path="contact" element={<Contact />} />
+                                </Routes>
+                            </Suspense>
+                            <Footer />
+                        </Router>
+                        
+                        {/* <UseScrollToTop /> */}
+                    </div>
+                </div>
+            </AnimatePresence>
+    );
 }
 
 export default App;
